b62152460794d299afd893119618fec2099a89b5
haneul-dev <kangheneul.dev@gmail.com>
2024-10-01 23:39:07 +0900

    7일차 문제 풀이 완료

---
+++ week2/problem25.py
@@ -0,0 +1,7 @@
+# 특정 문자 제거하기
+
+def solution(my_string, letter):
+    return my_string.replace(letter, "")
+
+
+# my_string 에서 letter 문자열 제거
---
+++ week2/problem28.py
@@ -0,0 +1,7 @@
+# 짝수의 합
+
+def solution(n):
+    return sum( i for i in range(0, n+1, 2))
+            
+    
+    
